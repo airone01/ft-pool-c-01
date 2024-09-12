77c013873d04a00d8529d1816a17e53d2a900013
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-12 10:29:31 +0200

    feat(ex06): implement

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <erwann.lagouche@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 10:24:30 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/12 10:29:27 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_strlen("hi"));
+// 	printf("%d\n", ft_strlen("Hello, world!"));
+}
