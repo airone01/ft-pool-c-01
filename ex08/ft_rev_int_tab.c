11eeaebe5c312597d629357db6be7227cb65bf1c
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-15 18:40:12 +0200

    fix(ex08): norm

--- ex08/ft_rev_int_tab.c
+++ ex08/ft_rev_int_tab.c
@@ -17,17 +17,17 @@ void	ft_sort_int_tab(int *tab, int size)
 	int	i;
 	int	j;
 	int	temp;
-	
+
 	i = 0;
 	while (i < (size - 1))
 	{
 		j = 0;
 		while (j < size - 1 - i)
 		{
-			if (tab[j] > tab[j+1])
+			if (tab[j] > tab[j + 1])
 			{
-				temp = tab[j+1];
-				tab[j+1] = tab[j];
+				temp = tab[j + 1];
+				tab[j + 1] = tab[j];
 				tab[j] = temp;
 			}
 			j++;
